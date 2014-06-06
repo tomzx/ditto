6410d4ecc3e947f405ac0645985a1770cb56d2b8
Tom Rochette <roctom@gmail.com>
2014-06-06 00:30:18 -0400

    - Return Ditto objects instead of keeping/using a "last_result" attribute. This fixes an issue where you could do something like $ditto->someMethod(); $ditto->someOtherMethod(); which would call someOtherMethod() on someMethod() output, which is not what one would expect. - Refactored Ditto::__call slightly to improve readability.

--- README.md
+++ README.md
@@ -6,6 +6,30 @@
 [![Latest Unstable Version](https://poser.pugx.org/tomzx/ditto/v/unstable.svg)](https://packagist.org/packages/tomzx/ditto)
 [![License](https://poser.pugx.org/tomzx/ditto/license.svg)](https://packagist.org/packages/tomzx/ditto)
 
+### Usage
+
+```php
+use Ditto\Ditto as d;
+
+$ditto = d::make('SomeClass');
+$ditto->someMethod()->shouldReturn('some value');
+
+// ===
+$ditto->someMethod()->shouldReturn('some value');
+$ditto->someMethod()->shouldBe('some value');
+$ditto->someMethod()->shouldEqual('some value');
+$ditto->someMethod()->shouldBeEqualTo('some value');
+
+// ==
+$ditto->someMethod()->shouldBeLike('some value');
+
+// instanceof
+$ditto->someMethod()->shouldHaveType('SomeType');
+$ditto->someMethod()->shouldReturnAnInstanceOf('SomeType');
+$ditto->someMethod()->shouldBeAnInstance('SomeType');
+$ditto->someMethod()->shouldImplement('SomeType');
+```
+
 ### License
 
 Ditto is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
--- src/Ditto/Ditto.php
+++ src/Ditto/Ditto.php
@@ -6,8 +6,6 @@ class Ditto
 {
 	protected $object;
 
-	protected $last_result;
-
 	protected static $container;
 
 	protected $test_keywords = [
@@ -25,13 +23,12 @@ class Ditto
 	private function __construct($object)
 	{
 		$this->object = $object;
-		$this->last_result = $object;
 	}
 
 	public static function make($instance)
 	{
 		self::initializeContainer();
-		if (gettype($instance) === 'string') {
+		if (is_string($instance)) {
 			// Check if we can make it
 			if (class_exists($instance)) {
 				$instance = self::$container->make($instance);
@@ -50,24 +47,23 @@ class Ditto
 
 	public function __call($method, array $arguments = [])
 	{
-		if (strpos($method, 'should') === 0) {
-			if (array_key_exists($method, $this->test_keywords)) {
-				$assert = $this->test_keywords[$method];
-				$this->$assert($arguments);
-				$this->last_result = $this->object;
-				return $this;
-			} else if (strpos($method, 'shouldBe') === 0) {
-				$method = 'is'.substr($method, strlen('shouldBe'));
-				// TODO: Check if method exists
-				\PHPUnit_Framework_Assert::assertTrue($this->object->$method());
-			} else 	if (strpos($method, 'shouldHave') === 0) {
-				$method = 'has'.substr($method, strlen('shouldHave'));
-				// TODO: Check if method exists
-				\PHPUnit_Framework_Assert::assertTrue($this->object->$method());
-			}
+		if (strpos($method, 'should') !== 0) {
+			$result = call_user_func_array(array($this->object, $method), $arguments);
+			return new self($result);
 		}
 
-		$this->last_result = call_user_func_array(array($this->last_result, $method), $arguments);
+		if (array_key_exists($method, $this->test_keywords)) {
+			$assert = $this->test_keywords[$method];
+			$this->$assert($arguments);
+		} else if (strpos($method, 'shouldBe') === 0) {
+			$method = 'is'.substr($method, strlen('shouldBe'));
+			// TODO: Check if method exists
+			\PHPUnit_Framework_Assert::assertTrue($this->object->$method());
+		} else if (strpos($method, 'shouldHave') === 0) {
+			$method = 'has'.substr($method, strlen('shouldHave'));
+			// TODO: Check if method exists
+			\PHPUnit_Framework_Assert::assertTrue($this->object->$method());
+		}
 
 		return $this;
 	}
@@ -75,26 +71,26 @@ class Ditto
 	public function __get($key)
 	{
 		// TODO: Check if property exists
-		return $this->last_result->$key;
+		return $this->object->$key;
 	}
 
 	public function __set($key, $value)
 	{
-		$this->last_result->$key = $value;
+		$this->object->$key = $value;
 	}
 
 	protected function assertSame($arguments)
 	{
-		\PHPUnit_Framework_Assert::assertSame($arguments[0], $this->last_result);
+		\PHPUnit_Framework_Assert::assertSame($arguments[0], $this->object);
 	}
 
 	protected function assertEquals($arguments)
 	{
-		\PHPUnit_Framework_Assert::assertEquals($arguments[0], $this->last_result);
+		\PHPUnit_Framework_Assert::assertEquals($arguments[0], $this->object);
 	}
 
 	protected function assertInstanceOf($arguments)
 	{
-		\PHPUnit_Framework_Assert::assertInstanceOf($arguments[0], $this->last_result);
+		\PHPUnit_Framework_Assert::assertInstanceOf($arguments[0], $this->object);
 	}
 }
